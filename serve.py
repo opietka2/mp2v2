6e528f8176d28937c52184193f5e4a3f48ce3489
opietka2 <133262948+opietka2@users.noreply.github.com>
2023-05-11 21:08:35 +0200

    Add files via upload

--- serve.py
+++ serve.py
@@ -8,6 +8,7 @@ app = Flask(__name__)
 @app.route('/', methods = ['POST'])
 def stress():
    subprocess.Popen([sys.executable, "stress_cpu.py"])
+   return '', 204
 
 @app.route('/', methods = ['GET'])
 def get_ip():
@@ -15,4 +16,4 @@ def get_ip():
    return host
 
 if __name__ == '__main__':
-   app.run(debug=True)
+   app.run(debug=True, host="0.0.0.0", port=port)
